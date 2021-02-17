51c72e1848e727c2e8ba8d445d225923a05baf56
benn <benneumen@gmail.com>
2021-02-17 14:35:50 +0200

    changed file

--- Create_text_file.py
+++ Create_text_file.py
@@ -1,8 +1,4 @@
 with open('/Users/benn/Desktop/text_file.txt', 'w') as f:
-<<<<<<< HEAD
     data = 'Hey Ben'
-=======
-    data = 'some data to be written to the file'
->>>>>>> a5f8da02f1dc26c483e285c971f102b7fec1d592
     f.write(data)
 
