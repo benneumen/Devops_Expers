a1149d93b5c37c931b85f472152a7ca65aea7758
benn <benneumen@gmail.com>
2021-02-26 17:51:05 +0200

    added error handling routes

--- web_app.py
+++ web_app.py
@@ -22,5 +22,25 @@ def stop_server():
     except:
         return {'status': 'error', 'reason': 'server not responding'}, 500
 
+class InvalidUsage(Exception):
+    status_code = 400
 
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 app.run(host='127.0.0.1', debug=True, port=5001)
